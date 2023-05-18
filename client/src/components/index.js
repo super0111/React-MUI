2f633c152d34113fd653e2864921e4b25f7ebaa4
*SUPER* <adonis0923dev@gmail.com>
2023-05-18 20:03:23 +0200

    code

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,4 @@
 export { default as Navbar } from "./Navbar";
-// export { default as Dropdown } from "./Dropdown";
 export { InputEmailField } from "./InputEmailField";
 export { FeedbackForm } from "./FeedbackForm";
 export { FAQs } from "./FAQs"
